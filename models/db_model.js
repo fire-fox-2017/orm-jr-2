99eb40266a62fdce17598fc22a470c4fc8a0456c
ridho0 <ridho230994@gmail.com>
2017-04-11 21:41:45 +0700

    selesai

--- models/db_model.js
+++ models/db_model.js
@@ -1,7 +1,34 @@
 "use strict"
+const repl = require('repl')
+const sqlite3 = require('sqlite3').verbose()
 
 class DBModel {
+  constructor(file) {
+    this.connection = new sqlite3.Database(file)
+  }
+  setup() {
+    let db = this.connection,
+        CREATE_TABLE_STUDENT = "CREATE TABLE IF NOT EXISTS student(id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL UNIQUE, last_name TEXT, cohort_id, FOREIGN KEY(cohort_id) REFERENCES cohort(id))",
+        CREATE_TABLE_COHORT = "CREATE TABLE IF NOT EXISTS cohort(id INTEGER PRIMARY KEY AUTOINCREMENT, cohort_name)"
 
+    db.serialize( () => {
+      db.run(CREATE_TABLE_STUDENT, (err) => {
+        if(!err) {
+          console.log(`table student berhasil dibuat`);
+        } else {
+          console.log(err.message);
+        }
+      })
+      db.run(CREATE_TABLE_COHORT, (err) => {
+        if(!err) {
+          console.log(`table cohort berhasil dibuat`);
+        } else {
+          console.log(err.message);
+        }
+      })
+    })
+
+  }
 
 }
 
--- models/student.js
+++ models/student.js
@@ -1,7 +1,114 @@
 "use strict"
 
 class Student {
+  constructor(first_name, last_name, cohort_id, id) {
+    this._fName = first_name
+    this._lName = last_name
+    this._cohortId = cohort_id
+    this._id = id
+  }
 
+  static create(model, obj) {
+    let db = model,
+        SEED_DATA_STUDENT = `INSERT INTO student(first_name, last_name, cohort_id) VALUES ('${obj._fName}', '${obj._lName}', '${obj._cohortId}' )`
+    db.serialize( () => {
+      db.run(SEED_DATA_STUDENT, (err) => {
+        if (!err) {
+          console.log(`'${obj._fName}' berhasil diSeed !`);
+        } else {
+          console.log(err.message);
+        }
+      })
+    })
+  }
+
+  static update(model, obj) {
+    let db = model,
+        UPDATE_DATA = `UPDATE student SET first_name = '${obj._fName}', last_name = '${obj._lName}', cohort_id = '${obj._cohortId}' WHERE id = '${obj._id}';`
+    db.serialize(() => {
+      db.run(UPDATE_DATA, (err) => {
+        if (!err) {
+          console.log(`'${obj._fName}' berhasil diUpdate !`);
+        } else {
+          console.log(err.message);
+        }
+      })
+    })
+  }
+
+  static delete(model, id) {
+    let db = model,
+        DELETE_DATA = `DELETE FROM student WHERE id = '${id}'`
+    db.serialize( () => {
+      db.run(DELETE_DATA, (err) => {
+        if (!err) {
+          console.log(`Data berhasil diDelete !`);
+        } else {
+          console.log(err.message);
+        }
+      })
+    })
+  }
+
+  static findById(db, id) {
+    let FIND_DATA = `SELECT * FROM student WHERE id = '${id}'`
+    db.serialize( () => {
+      db.all(FIND_DATA, (err, data) => {
+        if (!err) {
+          console.log(data)
+        } else {
+          console.log(err.message)
+        }
+      })
+    })
+  }
+
+  static where(db, first_name, callback) {
+    let FIND_DATA = `SELECT * FROM student WHERE ${first_name}`
+    db.serialize( () => {
+      db.all(FIND_DATA, (err, data) => {
+        if (!err) {
+          callback(data, null)
+        } else {
+          callback(null, err)
+        }
+      })
+    })
+  }
+
+  static findAll(db, obj, callback) {
+    let FIND_ALL = `SELECT * FROM student LIMIT ${obj.limit} OFFSET ${obj.offset}`
+    db.serialize(function() {
+      db.all(FIND_ALL, function(err, data) {
+        if(err) {
+          callback(null, err)
+        } else {
+          callback(data, null)
+        }
+      })
+    })
+  }
+
+  static findOrCreate(db, obj) {
+    let SELECT_DATA = `SELECT * FROM student WHERE last_name = '${obj._lName}'`
+    let INSERT_DATA = `INSERT INTO student(cohort_id, first_name, last_name) VALUES (${obj._cohortId}, '${obj._fName}', '${obj._lName}')`
+
+    db.serialize(function() {
+      db.all(SELECT_DATA, function(err, data) {
+        if(data.length){
+          console.log(`data '${obj._fName}' '${obj._lName}' sudah ada!`)
+        } else {
+          db.run(INSERT_DATA, function(err) {
+            if(err){
+              console.log(err)
+            } else {
+              console.log(`data '${obj._fName}' '${obj._lName}' berhasil ditambahkan`)
+            }
+          })
+        }
+      })
+    })
+  }
 
 }
 
