d918a134100ca2bea332afc3e40357df0e8a1754
stedyyulius <stedyyulius@gmail.com>
2017-04-16 14:43:15 +0700

    stelah dual boot jd eror

--- index.js
+++ index.js
@@ -3,3 +3,37 @@
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+
+let repl = require("repl");
+let replServer = repl.start({prompt: "> "});
+let dbModel = new DBModel ("db/student.db")
+replServer.context.dbModel = dbModel
+
+replServer.context.Student = Student;
+
+// Student.findAll(dbModel.connection,{limit: 2, offset: 1}), function(data,err){
+//   if(!err){
+//     for(var i =0; i < data.length; i++){
+//       console.log(data[i]);
+//     } else {
+//       console.log("Error");
+//     }
+//   }
+// })
+
+
+// Student.findOrCreate(dbModel.connection, new Student("Windiana", "Krismanuyar",1))
+
+//Cohort.findAll(dbModel.connection,{limit: 2, offset: 1}), function(data,err) {
+//   if(!err){
+//     for(let i = 0; i < data.length; i++){
+//       console.log(data[i]);
+//     } else {
+//         console.log("Error");
+//       }
+//     }
+//   }
+// })
+
+
+// Cohort.findOrCreate(dbModel.connection, new Cohort("Windiana", "Krismanuyar",1))
--- models/db_model.js
+++ models/db_model.js
@@ -1,8 +1,67 @@
 "use strict"
+// const repl = require("repl")
+// const sqlite = require("sqlite3").verbose();
+//
+//const repl = require("repl");
+const sqlite = require("sqlite3").verbose();
+
+// var file = "./db/student.db";
+// var db = new sqlite3.Database(file);
+
+// var CREATE_TABLEs = "CREATE TABLE IF NOT EXIST student (id INTERGER PRIMARY KEY AUTOINCREMENT, Firstname VARCHAR(50), Lastname VARCHAR(50), id_cohort varchar(10))"
+// var CREATE_TABLEc = "CREATE TABLE IF NOT EXIST cohort ()"
+// var SEED_DATA = "INSERT INTO students (firstname, lastname) VALUES ('Stedy', 'Yulius'), ('Riza', 'Fahmi');";
 
 class DBModel {
+  constructor(connection) {
+    this.connection = new sqlite.Database(connection);
+  }
 
+  setup() {
+    let db = this.connection;
+    let create = "CREATE TABLE IF NOT EXISTS student (id_student INTEGER PRIMARY KEY AUTOINCREMENT, firstname VARCHAR(50), lastname VARCHAR(50), cohort_id INTEGER,FOREIGN KEY(cohort_id) REFERENCES cohort(id_cohort))"
+    let createCohort = "CREATE TABLE IF NOT EXISTS cohort (id_cohort INTEGER PRIMARY KEY AUTOINCREMENT, cohort_name VARCHAR(50), created_at VARCHAR(50))"
+    db.serialize(function() {
+      db.run(create, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Table Created student!");
+        }
+      });
 
+      db.run(createCohort, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Table Created cohort!");
+        }
+      });
+    });
+  }
 }
+
+
+
+
+// let seedData = () => {
+// db.serialize(function() {
+//   db.run(SEED_DATA, function(err) {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       console.log('successfully seed data to table students');
+//       }
+//     });
+//   });
+// }
+
+
+
+// var replServer = repl.start('> ');
+
+// replServer.context.createTable = createTable;
+// replServer.context.seedData = seedData;
+//
 
 export default DBModel
